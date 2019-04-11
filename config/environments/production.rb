31afdf72f2dd9ab9e5fd71e3f65eb679e303f189
Ian Coker <xcoker@gmail.com>
2019-04-11 06:13:11 +0000

    atempting fix for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
